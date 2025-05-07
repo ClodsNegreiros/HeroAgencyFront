c516416a8c0612481c5641a30c4807ab127a05b4
ClodsNegreiros <clodsn@gmail.com>
2025-05-07 14:32:28 -0300

    feat: adicionado tela de detalhes de super heroi ao route.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroFormComponent } from './pages/hero/components/hero-form/hero-form.component';
 import { HeroListComponent } from './pages/hero/components/hero-list/hero-list.component';
+import { HeroDetailComponent } from './pages/hero/components/hero-detail/hero-detail.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/heroes', pathMatch: 'full' },
     { path: 'heroes/new', component: HeroFormComponent },
     { path: 'heroes/edit/:id', component: HeroFormComponent },
     { path: 'heroes', component: HeroListComponent },
+    { path: 'heroes/:id', component: HeroDetailComponent }
 ];
 
 @NgModule({
