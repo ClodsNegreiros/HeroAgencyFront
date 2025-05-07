82dae595800cbaece0384af9c5f38a389fd0d53b
ClodsNegreiros <clodsn@gmail.com>
2025-05-07 14:17:24 -0300

    feat: implementado app.routes e registrando tela de cadastro de heroes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroFormComponent } from './pages/hero/components/hero-form/hero-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/heroes', pathMatch: 'full' },
+    { path: 'heroes/new', component: HeroFormComponent },
+    { path: 'heroes/edit/:id', component: HeroFormComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { } 
