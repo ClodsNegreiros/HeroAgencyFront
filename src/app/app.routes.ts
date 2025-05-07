97aefc82aedf2ebdb3742fac3c31b702e2f59ea8
ClodsNegreiros <clodsn@gmail.com>
2025-05-07 14:22:37 -0300

    feat: adicionado tela de listagem de heróis ao route.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroFormComponent } from './pages/hero/components/hero-form/hero-form.component';
+import { HeroListComponent } from './pages/hero/components/hero-list/hero-list.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/heroes', pathMatch: 'full' },
     { path: 'heroes/new', component: HeroFormComponent },
     { path: 'heroes/edit/:id', component: HeroFormComponent },
+    { path: 'heroes', component: HeroListComponent },
 ];
 
 @NgModule({
